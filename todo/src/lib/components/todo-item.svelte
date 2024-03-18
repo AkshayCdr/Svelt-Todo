45f8e19aeeb98e3b4abf9812248e0239d180faed
akshay <akshaychandra418@gmail.com>
2024-03-18 11:05:36 +0530

    added html structure

---
+++ todo/src/lib/components/todo-item.svelte
@@ -0,0 +1,13 @@
+<div class="todo">
+  <form>
+    <input type="text" name="priority" />
+    <input type="checkbox" name="completed" />
+    <input type="text" name="name" />
+  </form>
+  <form>
+    <input type="date" name="date" />
+    <textarea name="description" cols="30" rows="10"></textarea>
+  </form>
+  <button>save</button>
+  <button>delete</button>
+</div>
