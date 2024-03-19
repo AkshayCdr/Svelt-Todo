a7d7a27d8d9d323abb44669598a7020abc099a66
akshay <akshaychandra418@gmail.com>
2024-03-19 15:57:03 +0530

    added css styles

--- todo/src/lib/components/todo-item.svelte
+++ todo/src/lib/components/todo-item.svelte
@@ -65,30 +65,150 @@
 
 <div class="todo">
   <form on:submit|preventDefault={handleSubmit}>
-    <!-- <input type="text" name="priority" value={priority} />
-     -->
-    <select name="priority" bind:value={priority}>
-      {#each priorityOptions as option}
-        <option value={option}>{option}</option>
-      {/each}
-    </select>
-    <input
-      type="checkbox"
-      name="completed"
-      bind:checked={completed}
-      on:change={handleComplete}
-    />
-    <input type="text" name="name" value={name} />
-    <input type="date" name="date" value={date ? date.split("T")[0] : null} />
-    <textarea name="description" cols="30" rows="10" value={description}
-    ></textarea>
-    <input type="submit" value="submit" />
+    <div class="main">
+      <select name="priority" bind:value={priority} class="task-priority">
+        <option value=""></option>
+        {#each priorityOptions as option}
+          <option value={option}>{option}</option>
+        {/each}
+      </select>
+      <input
+        type="checkbox"
+        name="completed"
+        class="task-checkbox"
+        bind:checked={completed}
+        on:change={handleComplete}
+      />
+      <input type="text" name="name" value={name} class="task-name" />
+    </div>
+    <div class="addons">
+      <input
+        type="date"
+        class="task-date"
+        name="date"
+        value={date ? date.split("T")[0] : null}
+      />
+      <textarea
+        name="description"
+        class="task-description"
+        cols="10"
+        rows="10"
+        value={description}
+      ></textarea>
+      <div class="buttons">
+        <input type="submit" value="submit" class="btn btn-submit" />
+        <button on:click={handleDeleteClick} class="btn btn-delete"
+          >delete</button
+        >
+      </div>
+    </div>
   </form>
-  <button on:click={handleDeleteClick}>delete</button>
 </div>
 
 <style>
   .todo {
-    border: 1px solid black;
+    /* border: 1px solid black; */
+    margin: 10px 10px;
+    padding: 5px 5px;
+    border-radius: 20px;
+    background-color: teal;
+    box-sizing: border-box;
+  }
+
+  .todo:hover {
+    box-shadow: rgba(0, 0, 0, 0.56) 0px 22px 70px 4px;
+  }
+
+  .todo .main {
+    margin: 10px 10px;
+    display: flex;
+    align-items: center;
+  }
+
+  .todo .task-priority {
+    min-width: 30px;
+    min-height: 30px;
+    font-size: 18px;
+    border: none;
+    background: transparent;
+    color: wheat;
+  }
+
+  .todo .task-checkbox {
+    margin: 0 7px;
+    /* border-radius: 50px; */
+    color: beige;
+    width: 2em;
+    height: 2em;
+    border: 0.15em solid beige;
+    border-radius: 0.15em;
+    transform: translateY(-0.075em);
+  }
+
+  /* input[type="checkbox"] {
+    outline: 3px solid black;
+  } */
+
+  .todo .task-name {
+    min-height: 30px;
+    font-size: 20px;
+    border: none;
+    background: transparent;
+    margin: 0 5px;
+    padding: 5px 5px;
+    color: white;
+  }
+
+  .todo .addons {
+    margin: 10px 10px;
+    /* display: flex; */
+    /* border: 1px solid black; */
+    display: flex;
+    flex-direction: column;
+    box-sizing: border-box;
+    gap: 10px;
+  }
+
+  .todo .addons .task-date {
+    min-height: 40px;
+    width: 300px;
+    border: 5px solid darkblue;
+    border-radius: 10px;
+    background: transparent;
+    font-size: 18px;
+  }
+
+  .todo .addons .task-description {
+    max-height: 150px;
+    border: 5px solid yellow;
+    border-radius: 10px;
+    background: transparent;
+    font-size: 18px;
+  }
+
+  .todo .addons .buttons {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .todo .addons .buttons .btn {
+    width: 90px;
+    height: 40px;
+    font-size: 17px;
+    border-radius: 10px;
+  }
+
+  .btn-submit {
+    background-color: rgb(6, 86, 156);
+    color: white;
+  }
+
+  .btn-delete {
+    background-color: rgb(163, 7, 36);
+    color: white;
+  }
+
+  .hide {
+    display: none !important;
   }
 </style>
--- todo/src/routes/+page.svelte
+++ todo/src/routes/+page.svelte
@@ -12,7 +12,6 @@
       method: "GET",
       headers: { "Content-Type": "application/json" },
     });
-    console.log(response);
     if (!response.ok) return new Error("Error submitting");
     todos = await response.json();
     todos = [...todos];
@@ -31,22 +30,71 @@
   }
 </script>
 
-<form on:submit|preventDefault={handleSubmit}>
-  <input type="text" name="task" placeholder="add task" />
-  <input type="submit" value="add task" />
-</form>
+<div class="container">
+  <div class="input">
+    <form on:submit|preventDefault={handleSubmit}>
+      <input type="text" name="task" placeholder="add task" class="task-name" />
+      <input type="submit" value="add task" class="btn btn-submit" />
+    </form>
+  </div>
 
-<div>
-  {#each todos as todo (todo.id)}
-    <TodoItem
-      id={todo.id}
-      priority={todo.priority}
-      name={todo.name}
-      description={todo.description}
-      completed={todo.completed}
-      date={todo.date}
-      onDelete={populateTodo}
-      onSubmit={populateTodo}
-    />
-  {/each}
+  <div>
+    {#each todos as todo (todo.id)}
+      <TodoItem
+        id={todo.id}
+        priority={todo.priority}
+        name={todo.name}
+        description={todo.description}
+        completed={todo.completed}
+        date={todo.date}
+        onDelete={populateTodo}
+        onSubmit={populateTodo}
+      />
+    {/each}
+  </div>
 </div>
+
+<style>
+  .container {
+    max-width: fit-content;
+    margin: 20px auto;
+    padding: 20px 20px;
+    border: 1px solid black;
+    /* background-image: linear-gradient(to right, #d7d2cc 0%, #304352 100%); */
+    background-color: rgb(91, 5, 69);
+    border-radius: 13px;
+  }
+
+  .input {
+    border: 1px solid black;
+    background-color: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px 10px;
+    padding: 20px;
+    border-radius: 20px;
+    max-height: 30px;
+  }
+
+  .input .task-name {
+    min-width: 500px;
+    margin: 0 5px;
+    min-height: 40px;
+    border-radius: 20px;
+    text-align: center;
+    border: none;
+    background-color: transparent;
+    font-size: 20px;
+    color: white;
+  }
+
+  .btn-submit {
+    min-width: 100px;
+    min-height: 40px;
+    background-color: dodgerblue;
+    border-radius: 20px;
+    color: white;
+    font-size: medium;
+  }
+</style>
